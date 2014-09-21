7dff6046ee6686106e8db5e9b30a093c2c2b7b93
Baraa Katbeh <b.katbeh@gmail.com>
2014-09-21 10:50:34 +0300

    first check-in

---
+++ trunk/SampleApplication/Bank/UI/_js/webServicesReferences.js
@@ -0,0 +1,10 @@
+
+var serverName = '/';
+
+var getEventsInfo = serverName + 'RTDWS/MessagesService?svc=getEventsInfo';
+var reset = serverName + 'RTDWS/MessagesService?svc=reset';
+var dataReset = serverName + 'RTDWS/MessagesService?svc=dataReset';
+
+var start = serverName + 'RTDWS/MessagesService?svc=start';
+
+var SendSms = serverName + 'ServicePrototype/SendSms';
