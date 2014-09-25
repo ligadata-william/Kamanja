b47c33ed4de4aec1072d1b8cb189087ea10b261f
ligadata-ramana <ramana@ligadata.com>
2014-09-24 23:11:27 -0700

    OpenDbStore changed

--- trunk/MetadataAPIService/src/main/scala/com/ligadata/metadataapiservice/Boot.scala
+++ trunk/MetadataAPIService/src/main/scala/com/ligadata/metadataapiservice/Boot.scala
@@ -29,7 +29,8 @@ object Boot extends App {
     MdMgr.GetMdMgr.truncate
     val mdLoader = new com.ligadata.olep.metadataload.MetadataLoad (MdMgr.mdMgr, logger,"","","","")
     mdLoader.initialize
-    MetadataAPIImpl.OpenDbStore
+    MetadataAPIImpl.readMetadataAPIConfig
+    MetadataAPIImpl.OpenDbStore(MetadataAPIImpl.GetMetadataAPIConfig.getProperty("DATABASE"))
     MetadataAPIImpl.LoadObjectsIntoCache
 
     val callbackActor = actor(new Act {
