2518cab9244c717f85d1d375fdde7c4eea6425e9
ligadata-william <william@ligadata.com>
2014-10-31 09:10:56 -0700

    Minor changes

--- trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/TestMetadataAPI.scala
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/TestMetadataAPI.scala
@@ -60,7 +60,7 @@ object TestMetadataAPI{
   }
 
   
-  // Types
+  // Type defs
   
   def AddType {
     try {
@@ -210,8 +210,9 @@ object TestMetadataAPI{
     }
   }
   
-  // End Types
+  // End Type defs
   
+  // TODO: Rewrite Update Type to allow a user to pick the file they wish to update a type from.
   def UpdateType = {
     val apiResult = MetadataAPIImpl.UpdateType(SampleData.sampleNewScalarTypeStr,"JSON")
     val (statusCode,resultData) = MetadataAPIImpl.getApiResult(apiResult)
