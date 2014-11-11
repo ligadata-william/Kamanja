185316ed8e07040b9b65f6cb5482aef1e3a4bb98
Pokuri <pokuri@ligadata.com>
2014-11-11 11:30:36 -0800

    Added ImmutableMapTypeDef to zkSerializeObjectToJson

--- trunk/Utils/Serialize/src/main/scala/com/ligadata/Serialize/JsonSerializer.scala
+++ trunk/Utils/Serialize/src/main/scala/com/ligadata/Serialize/JsonSerializer.scala
@@ -667,6 +667,17 @@ object JsonSerializer {
 		      ("DependantJars"   -> o.dependencyJarNames.toList))
 	  pretty(render(json))
 	}
+	case o:ImmutableMapTypeDef => {
+	  val json = (("ObjectType"      -> "ImmutableMapTypeDef") ~
+		      ("Operation"       -> operation) ~
+		      ("NameSpace"       -> o.nameSpace) ~
+		      ("Name"            -> o.name) ~
+		      ("Version"         -> o.ver) ~
+		      ("PhysicalName"    -> o.physicalName) ~
+		      ("JarName"         -> o.jarName) ~
+		      ("DependantJars"   -> o.dependencyJarNames.toList))
+	  pretty(render(json))
+	}
 	case _ => {
             throw new UnsupportedObjectException("zkSerializeObjectToJson doesn't support the  objects of type objectType of " + mdObj.getClass().getName() + " yet.")
 	}
