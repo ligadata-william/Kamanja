814164cacff134c74e73aac22bb77e4d59376bf1
ligadata-tulasi <tulasi@ligadata.com>
2014-11-22 08:46:16 -0800

    Added populate json for arrays

--- trunk/MessageDef/src/main/scala/com/ligadata/messagedef/MessageDefImpl.scala
+++ trunk/MessageDef/src/main/scala/com/ligadata/messagedef/MessageDefImpl.scala
@@ -221,6 +221,17 @@ class MessageDefImpl {
     }
     addMessageFunc
   }
+  
+  private def assignJsonForArray(fname : String, typeImpl:String) = {
+    """
+    if (map.getOrElse(""""+fname+"""", null).isInstanceOf[tList])
+        arr = map.getOrElse(""""+fname+"""", null).asInstanceOf[List[String]]
+    if (arr != null) {
+        """+fname+"""  = arr.map(v => """+typeImpl+"""(v)).toArray
+    }
+    """
+  }
+  
   //generates the variables string and assign string
   def classStr(message: Message, mdMgr: MdMgr): (String, String, String, String, Int, List[(String, String)], List[(String, String, String, String, Boolean, String)], String) = {
     var scalaclass = new StringBuilder(8 * 1024)
@@ -276,10 +287,12 @@ class MessageDefImpl {
                 else
                   fname = arrayType.elemDef.implementationName + ".Input"
                 assignCsvdata.append("%s%s = list(inputdata.curPos).split(arrvaldelim, -1).map(v => %s(v));\n%sinputdata.curPos = inputdata.curPos+1\n".format(pad2, f.Name, fname, pad2))
+                           
+                 assignJsondata.append( assignJsonForArray( f.Name, fname))
               } else {
-                if (arrayType.elemDef.tTypeType.toString().toLowerCase().equals("tcontainer"))
+                if (arrayType.elemDef.tTypeType.toString().toLowerCase().equals("tcontainer")){
                   assignCsvdata.append(newline + getArrayStr(f.Name, arrayType.elemDef.physicalName) + newline + "\t\tinputdata.curPos = inputdata.curPos+1" + newline)
-
+                }
                 if (arrayType.elemDef.tTypeType.toString().toLowerCase().equals("tmessage"))
                   if (typ.get.typeString.toString().split("\\[").size == 2) {
                     addMsg.append(pad2 + "if(curVal._2.compareToIgnoreCase(\"" + f.Name + "\") == 0) {" + newline + "\t")
@@ -347,13 +360,11 @@ class MessageDefImpl {
 
                 argsList = (message.NameSpace, f.Name, arrayBufType.NameSpace, arrayBufType.Name, false, null) :: argsList
                 scalaclass = scalaclass.append("%svar %s: %s = new %s;%s".format(pad1, f.Name, typ.get.typeString, typ.get.typeString, newline))
-                if (f.ElemType.equals("Container"))
+                if (f.ElemType.equals("Container")) {
                   assignCsvdata.append("%s//%s Implementation of messages is not handled at this time%s".format(pad2, f.Name, newline))
-
-                // assignCsvdata.append(newline + getArrayStr(f.Name, arrayBufType.elemDef.physicalName) + newline + "\t\tinputdata.curPos = inputdata.curPos+1" + newline)
-
-                // assignCsvdata.append("%s//%s Implementation of messages is not handled at this time".format(pad2, f.Name))
-
+                // assignJsondata.append("%s%s.assignJsonData(map)%s".format(pad1, f.Name, newline))
+                  // assignCsvdata.append(newline + getArrayStr(f.Name, arrayBufType.elemDef.physicalName) + newline + "\t\tinputdata.curPos = inputdata.curPos+1" + newline)
+                }
                 if (typ.get.typeString.toString().split("\\[").size == 2) {
                   addMsg.append(pad2 + "if(curVal._2.compareToIgnoreCase(\"" + f.Name + "\") == 0) {" + newline + "\t")
                   addMsg.append(pad3 + f.Name + " += msg.asInstanceOf[" + typ.get.typeString.toString().split("\\[")(1) + newline + pad3 + "} else ")
@@ -398,8 +409,7 @@ class MessageDefImpl {
                     jarset = jarset + msgDef.JarName
 
                   argsList = (f.NameSpace, f.Name, typ.get.NameSpace, typ.get.Name, false, null) :: argsList
-                
-                  
+
                   /*
         	Messages in Message is not handled at this moment
        
@@ -719,7 +729,9 @@ class XmlData(var dataInput: String) extends InputData(){ }
 
   def assignJsonData(assignJsonData: String) = {
     """
-  private def assignJsonData(map:Map[String,Any]) : Unit =  {
+	private def assignJsonData(map:Map[String,Any]) : Unit =  {
+    type tList = List[String]
+    var arr: List[String] = null
     try{
 	  if(map == null)  throw new Exception("Invalid json data")
 """ + assignJsonData +
