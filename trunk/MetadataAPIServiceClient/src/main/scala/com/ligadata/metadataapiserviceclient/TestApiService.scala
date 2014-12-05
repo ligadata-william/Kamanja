9baa9362bfd47fd5004eaf4b67f85c5a5b776336
ligadata-kunal <kunal@ligadata.com>
2014-12-05 15:03:20 -0800

    REST api - Message
    REST apis for Messages

--- trunk/MetadataAPIServiceClient/src/main/scala/com/ligadata/metadataapiserviceclient/TestApiService.scala
+++ trunk/MetadataAPIServiceClient/src/main/scala/com/ligadata/metadataapiserviceclient/TestApiService.scala
@@ -4,14 +4,13 @@ import uk.co.bigbeeconsultants.http._
 import uk.co.bigbeeconsultants.http.response._
 import uk.co.bigbeeconsultants.http.request._
 import uk.co.bigbeeconsultants.http.header.MediaType._
-
 import java.net.URL
 import org.apache.log4j._
 import org.json4s._
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
-
 import scala.io._
+
 case class APIResultInfo(statusCode:Int, statusDescription: String, resultData: String)
 case class APIResultJsonProxy(APIResults: APIResultInfo)
 
@@ -45,14 +44,21 @@ object GetModel {
   }
 
   def GetResponse(url:String,body: Option[String]): String = {
-    val httpClient = new HttpClient
+    val config = Config(connectTimeout = 10000,readTimeout = 30000)
+    val httpClient = new HttpClient(config)
     var response:Response = null
+    logger.trace("URL :" + url)
     if( body == None){
       response = httpClient.get(new URL(url))
     }
-    else{
+    else{     
+      if( body.get.equalsIgnoreCase("DELETE")){
+      response = httpClient.delete(new URL(url)) 
+      }
+      else{
       val requestBody = new StringRequestBody(body.get,TEXT_XML)
       response = httpClient.put(new URL(url),requestBody)
+      }
     }
     logger.trace("response.status => " + response.status)
     logger.trace("response.body   => " + response.body.asString)
@@ -65,14 +71,53 @@ object GetModel {
   def main(args: Array[String]) {
     logger.setLevel(Level.TRACE);
     val httpClient = new HttpClient
+    
+    val dirName = "/Users/kunal/Desktop/RTD/trunk/MetadataAPI/src/test/SampleTestFiles"
+   /*
+      // *********************** Model functions ************************
+    //GetAllModelDefs
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/GetAllModelDefs/JSON",None))
+    
+    //GetModelDef
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/GetModelDef",None))
+    
     // GetModel
-    logger.trace(GetResponse("http://127.0.0.1:8080/api/GetModel/system/copdriskassessment_000100/100",None))
-    // GetType
-    logger.trace(GetResponse("http://127.0.0.1:8080/api/GetType/arrayoflong",None))
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/GetModel/system/copdriskassessment_000100/100",None))
+  
     // AddModel
-    val dirName = "/home/vmandava/github/RTD/trunk/MetadataAPI/src/test/SampleTestFiles/Models"
-    val pmmlFilePath = dirName + "/COPD_V200.xml"
+    val pmmlFilePath = dirName + "/Models/COPD.xml"
     val pmmlStr = Source.fromFile(pmmlFilePath).mkString    
-    //logger.trace(GetResponse("http://127.0.0.1:8080/api/AddModel",Some(pmmlStr)))
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/AddModel",Some(pmmlStr)))
+    
+    //RemoveModel
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/RemoveModel",Some("DELETE")))
+    
+    //UpdateModel
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/UpdateModel",None))
+    */
+    
+    
+   // ******************** Message functions **************************
+    //GetAllMessageDefs
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/GetAllMessageDefs/JSON",None))
+    
+    //GetMessageDef
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/GetMessageDef/hl7/JSON",None))
+
+	//GetMessageDef
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/GetMessageDef/hl7/100/JSON",None))
+  
+    //AddMessage
+    val messageJsonPath = dirName + "/Messages/hl7.json"
+    val addMessageStr = Source.fromFile(messageJsonPath).mkString
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/AddMessageDef/JSON",Some(addMessageStr)))
+  
+    //RemoveMessage
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/RemoveMessage/hl7/100",Some("DELETE")))
+    
+    //UpdateMessage
+    val updateJsonPath = dirName + "/Messages/hl7.json"
+    val updateMessageStr = Source.fromFile(updateJsonPath).mkString
+    logger.trace(GetResponse("http://127.0.0.1:8081/api/UpdateMessage/JSON",Some(updateMessageStr)))   
   }
-}
+} 
