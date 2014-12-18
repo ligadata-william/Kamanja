ec769e4b382011eec34637f92d6d53b1912dc5b8
Rich Johnson <rich@ligadata.com>
2014-12-18 02:17:28 -0800

    Cluster install script now generates config files for each cluster node to be used by cluster start script to start cluster node.
    These config files are copied into place after tarballs are installed (each goes to its appropriate directory).

--- trunk/SampleApplication/Medical/InstallScript/src/main/scala/com/ligadata/installer/NodeInfoExtract.scala
+++ trunk/SampleApplication/Medical/InstallScript/src/main/scala/com/ligadata/installer/NodeInfoExtract.scala
@@ -10,10 +10,18 @@ class NodeInfoExtract(val metadataAPIConfig : String, val nodeConfigPath : Strin
 
 	MetadataAPIImpl.InitMdMgrFromBootStrap(metadataAPIConfig)
 	
+	var metadataStoreType : String = MetadataAPIImpl.GetMetadataAPIConfig.getProperty("MetadataStoreType")
+	var metadataSchemaName : String = MetadataAPIImpl.GetMetadataAPIConfig.getProperty("MetadataSchemaName")
+	var metadataLocation : String = MetadataAPIImpl.GetMetadataAPIConfig.getProperty("MetadataLocation")
+	
 	val result : String = MetadataAPIImpl.UploadConfig(Source.fromFile(nodeConfigPath).mkString)
 	//println(result)
 	
-	def extract : (Array[String],Array[(String,String)]) = {
+	def MetadataStoreType : String = metadataStoreType
+	def MetadataSchemaName : String = metadataSchemaName
+	def MetadataLocation : String = metadataLocation
+	
+	def extract : (Array[String], Array[(String,String,String)],Array[(String,String)]) = {
 		val nodeInfos : Map[String,NodeInfo] = MdMgr.GetMdMgr.Nodes
 		
 		/** 
@@ -21,13 +29,17 @@ class NodeInfoExtract(val metadataAPIConfig : String, val nodeConfigPath : Strin
 		 	some work must be done to create a concise set of node/install directory pairs... 
 		 	
 		 	Build a Set[String] of (nodeIp,installPath) pairs...
+		 	
 		 */	
-    val uniqueNodes : Array[String] = nodeInfos.values.map(info => info.nodeIpAddr).toSet.toSeq.sorted.toArray 
-    val uniqueNodePaths : Set[String] = nodeInfos.values.map(info => info.nodeIpAddr + "~" + info.jarPaths.head).toSet 
-		val nodeIpPathPairs : Array[(String,String)] = uniqueNodePaths.map(itm => (itm.split('~').head, itm.split('~').last)).toSeq.sorted.toArray
-		(uniqueNodes,nodeIpPathPairs)
+	    val ips : Array[String] = nodeInfos.values.map(info => info.nodeIpAddr).toSet.toSeq.sorted.toArray 
+	    val ipIdTargPaths : Array[(String,String,String)] = nodeInfos.values.map(info => {
+		      	(info.nodeIpAddr, info.nodeId, info.jarPaths.head.toString)}
+		    ).toSet.toSeq.sorted.toArray 
+	   
+	    val uniqueNodePaths : Set[String] = nodeInfos.values.map(info => info.nodeIpAddr + "~" + info.jarPaths.head).toSet 
+		val ipPathPairs : Array[(String,String)] = uniqueNodePaths.map(itm => (itm.split('~').head, itm.split('~').last)).toSeq.sorted.toArray
+		(ips, ipIdTargPaths, ipPathPairs)
 	}
-	
 }
 
 object NodeInfoExtract extends App {
@@ -68,6 +80,10 @@ NodeInfoExtract --MetadataAPIConfig  <MetadataAPI config file path>
               nextOption(map ++ Map('ipFileName -> value), tail)
             case "--ipPathPairFileName" :: value :: tail =>
               nextOption(map ++ Map('ipPathPairFileName -> value), tail)
+            case "--workDir" :: value :: tail =>
+              nextOption(map ++ Map('workDir -> value), tail)  
+            case "--ipIdCfgTargPathQuartetFileName" :: value :: tail =>
+              nextOption(map ++ Map('ipIdCfgTargPathQuartetFileName -> value), tail)  
             case option :: tail =>
               println("Unknown option " + option)
               println(usage)
@@ -82,10 +98,17 @@ NodeInfoExtract --MetadataAPIConfig  <MetadataAPI config file path>
         val clusterConfigurationName = if (options.contains('ClusterConfigurationName)) options.apply('ClusterConfigurationName) else null
         val ipFileName = if (options.contains('ipFileName)) options.apply('ipFileName) else null
         val ipPathPairFileName = if (options.contains('ipPathPairFileName)) options.apply('ipPathPairFileName) else null
+        val workDir = if (options.contains('workDir)) options.apply('workDir) else null
+        val ipIdCfgTargPathQuartetFileName = if (options.contains('ipIdCfgTargPathQuartetFileName)) options.apply('ipIdCfgTargPathQuartetFileName) else null
         
 
-        val reasonableArguments : Boolean = (metadataAPIConfig != null && metadataAPIConfig.size > 0 && 
-            nodeConfigPath != null && nodeConfigPath.size > 0 && ipFileName != null && ipFileName.size > 0 && ipPathPairFileName != null && ipPathPairFileName.size > 0 && clusterConfigurationName == null)
+        val reasonableArguments : Boolean = (metadataAPIConfig != null && metadataAPIConfig.size > 0 
+            							&& nodeConfigPath != null && nodeConfigPath.size > 0 
+            							&& ipFileName != null && ipFileName.size > 0 
+            							&& ipPathPairFileName != null && ipPathPairFileName.size > 0 
+            							&& workDir != null && workDir.size > 0 
+            							&& ipIdCfgTargPathQuartetFileName != null && ipIdCfgTargPathQuartetFileName.size > 0 
+            							&& clusterConfigurationName == null)
         if (! reasonableArguments) {
             println("Your arguments are not satisfactory...Usage:")
             println(usage)
@@ -93,32 +116,84 @@ NodeInfoExtract --MetadataAPIConfig  <MetadataAPI config file path>
         }
                     
    		val extractor : NodeInfoExtract = new NodeInfoExtract(metadataAPIConfig, nodeConfigPath)
-   		val (ips,ipPathPairs) : (Array[String], Array[(String,String)]) = extractor.extract 
+   		val (ips, ipIdTargPaths, ipPathPairs) : (Array[String], Array[(String,String,String)], Array[(String,String)]) = extractor.extract 
 		
-      writeFileIps(ipFileName, ips)
-      writeFilePairs(ipPathPairFileName, ipPathPairs)
+	    writeFileIps(s"$workDir/$ipFileName", ips)
+	    writeFilePairs(s"$workDir/$ipPathPairFileName" , ipPathPairs)
+	    writeNodeIdConfigs(workDir, ipIdCfgTargPathQuartetFileName, extractor, ipIdTargPaths)
 	}
 
-  private def writeFileIps(outputPath : String, ips : Array[String]) {
-    val file = new File(outputPath);
-    val bufferedWriter = new BufferedWriter(new FileWriter(file))
-    ips.foreach(ip => {
-        bufferedWriter.write(s"$ip\n")
-    })
-    bufferedWriter.close
-  }
+  	private def writeFileIps(outputPath : String, ips : Array[String]) {
+		val file = new File(outputPath);
+		val bufferedWriter = new BufferedWriter(new FileWriter(file))
+		ips.foreach(ip => {
+		    bufferedWriter.write(s"$ip\n")
+		})
+		bufferedWriter.close
+  	}
   
-  private def writeFilePairs(outputPath : String, ipPathPairs : Array[(String,String)]) {
-    val file = new File(outputPath);
-    val bufferedWriter = new BufferedWriter(new FileWriter(file))
-    ipPathPairs.foreach(pair => {
-        val ip : String = pair._1
-        val path : String = pair._2
-        bufferedWriter.write(s"$ip\n")
-        bufferedWriter.write(s"$path\n")
-    })
-    bufferedWriter.close
-  }
+	private def writeFilePairs(outputPath : String, ipPathPairs : Array[(String,String)]) {
+	    val file = new File(outputPath);
+	    val bufferedWriter = new BufferedWriter(new FileWriter(file))
+	    ipPathPairs.foreach(pair => {
+	        val ip : String = pair._1
+	        val path : String = pair._2
+	        bufferedWriter.write(s"$ip\n")
+	        bufferedWriter.write(s"$path\n")
+	    })
+	    bufferedWriter.close
+	}
+
+	/**
+	 * Write an engine config file for each id presented in the ids array. Write another text file
+	 * consisting of ids and config file paths.  Each ip, id, and config file path, and targetPath are written on
+	 * four lines, one value per line.  These are processed by the install script to distribute
+	 * the appropriate config file to the appropriate ip to the appropriate target path for each node found there.
+	 * 
+	 * These files will be distributed to the appropriate nodes during the installation.
+	 * 
+	 *  @param workDir the directory where the config files and the nodeId.txt file will be written
+	 *  @param idCfgFileName the name of the file that will track the nodeId/config file name line pairs
+	 *  @param extractor the NodeInfoExtract instance containing most of the config info.
+	 *  @param ipIdTargs the (ip,id, targetPath) triple values for the cluster being processed 
+	 */
+	private def writeNodeIdConfigs(workDir : String, ipIdCfgTargPathQuartetFileName : String, extractor : NodeInfoExtract, ipIdTargs : Array[(String,String,String)]) {
+	    
+	    val storeType : String = extractor.MetadataStoreType
+		val schemaName : String = extractor.MetadataSchemaName
+		val mdLoc : String = extractor.MetadataLocation
+		
+	    ipIdTargs.foreach(ipIdTargTriple => {
+			val (_,id,_) : (String,String,String) = ipIdTargTriple
+			
+	    	val nodeCfgPath : String = s"$workDir/node$id.cfg"
+	    	val file = new File(nodeCfgPath);
+	    	val bufferedWriter = new BufferedWriter(new FileWriter(file))
+	    	
+			bufferedWriter.write(s"# Node Information\n")
+			bufferedWriter.write(s"nodeId=$id\n")
+			bufferedWriter.write(s"\n")
+			bufferedWriter.write(s"#Storing metadata using MetadataStoreType, MetadataSchemaName & MetadataLocation\n")
+			bufferedWriter.write(s"MetadataStoreType=$storeType\n")
+			bufferedWriter.write(s"MetadataSchemaName=$schemaName\n")
+			bufferedWriter.write(s"MetadataLocation=$mdLoc\n")
+			
+			bufferedWriter.close
+	    })
+	    
+    	val nodeCfgPath : String = s"$workDir/$ipIdCfgTargPathQuartetFileName"
+    	val file = new File(nodeCfgPath);
+		val bufferedWriter = new BufferedWriter(new FileWriter(file))
+		ipIdTargs.foreach(ipIdTargTriple => {
+			val (ip,id,targPath) : (String,String,String) = ipIdTargTriple
+		    bufferedWriter.write(s"$ip\n")
+		    bufferedWriter.write(s"$id\n")
+		    bufferedWriter.write(s"$workDir/node$id.cfg\n")
+		    bufferedWriter.write(s"$targPath\n")
+		})
+		bufferedWriter.close
+	    
+	}
   
 }
 
