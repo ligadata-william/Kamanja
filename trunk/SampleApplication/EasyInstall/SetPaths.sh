5c393bdbe9f16e7302d2224864d2bea149a40b91
ligadata-tulasi <tulasi@ligadata.com>
2015-01-25 13:47:00 -0800

    Added scripts for Easy Install

---
+++ trunk/SampleApplication/EasyInstall/SetPaths.sh
@@ -0,0 +1,35 @@
+jar_full_path=$(which jar)
+
+if [ "$?" != "0" ]; then
+	echo "Not found java home directory."
+	exit 1
+fi
+
+scala_full_path=$(which scala)
+
+if [ "$?" != "0" ]; then
+	echo "Not found java home directory."
+	exit 1
+fi
+
+java_home=$(dirname $(dirname $jar_full_path))
+scala_home=$(dirname $(dirname $scala_full_path))
+install_dir=$(dirname $(dirname $(readlink -f "$0")))
+
+java_home_repl=$(echo $java_home | sed 's/\//\\\//g')
+scala_home_repl=$(echo $scala_home | sed 's/\//\\\//g')
+install_dir_repl=$(echo $install_dir | sed 's/\//\\\//g')
+
+# changing path in script files
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/script/CreateQueues_Template.sh > $install_dir/bin/CreateQueues.sh
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/script/InitKvStores_Template.sh > $install_dir/bin/InitKvStores.sh
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/script/StartEngine_Template.sh > $install_dir/bin/StartEngine.sh
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/script/StartMetadataAPI_Template.sh > $install_dir/bin/StartMetadataAPI.sh
+
+# changing path in config files
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/config/ClusterConfig_Template.json > $install_dir/config/ClusterConfig.json
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/config/EngineConfig_Template.properties > $install_dir/config/EngineConfig.properties
+sed "s/{InstallDirectory}/$install_dir_repl/g;s/{ScalaInstallDirectory}/$scala_home_repl/g;s/{JavaInstallDirectory}/$java_home_repl/g" $install_dir/template/config/MetadataAPIConfig_Template.properties > $install_dir/input/application1/metadata/config/MetadataAPIConfig.properties
+
+chmod 777 $install_dir/bin/*.*
+
---
+++ trunk/SampleApplication/EasyInstall/template/script/StartEngine_Template.sh
@@ -0,0 +1,2 @@
+java -Dlog4j.configuration=file:{InstallDirectory}/config/log4j.properties -jar {InstallDirectory}/bin/OnLEPManager-1.0 --config {InstallDirectory}/config/Engine1Config.properties
+
