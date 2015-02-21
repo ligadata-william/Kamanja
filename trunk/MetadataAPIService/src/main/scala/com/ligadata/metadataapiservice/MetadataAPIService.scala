dd6bae952218f2972aef52f59f92516918016954
Dan Kozin <dan@Dans-MacBook-Pro.local>
2015-02-20 18:16:28 -0800

    Fix invalid DELETE URL message

--- trunk/MetadataAPIService/src/main/scala/com/ligadata/metadataapiservice/MetadataAPIService.scala
+++ trunk/MetadataAPIService/src/main/scala/com/ligadata/metadataapiservice/MetadataAPIService.scala
@@ -104,7 +104,11 @@ trait MetadataAPIService extends HttpService {
           {
             val toknRoute = str.split("/")
             logger.info("DELETE "+str)
-            requestContext => processDeleteRequest(toknRoute(0), toknRoute(1).toLowerCase, requestContext)   
+            if (toknRoute.size != 2) {
+              requestContext => processDeleteRequest(toknRoute(0).toLowerCase, toknRoute(1).toLowerCase, requestContext)   
+            } else {
+              requestContext =>  requestContext.complete((new ApiResult(-1, "Unknown URL", "Unknown DELETE route")).toString)
+            }
           }
         }
       }
