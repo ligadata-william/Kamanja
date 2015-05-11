d5d7b3a128b2f1987785664c16d8d25c5d09c149
ligadata-kunal <kunal@ligadata.com>
2015-05-11 13:17:53 -0700

    Update StartMetadataAPI_Template.sh

--- trunk/SampleApplication/Medical/template/script/StartMetadataAPI_Template.sh
+++ trunk/SampleApplication/Medical/template/script/StartMetadataAPI_Template.sh
@@ -3,7 +3,7 @@
 ipport="8998"
 
 if [ "$1" != "debug" ]; then
-	java -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/input/application1/metadata/config/MetadataAPIConfig.properties
+	java -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/input/application-2-Healthcare/metadata/config/MetadataAPIConfig.properties
 else	
 	java -Xdebug -Xrunjdwp:transport=dt_socket,address="$ipport",server=y -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/input/application-2-Healthcare/metadata/config/MetadataAPIConfig.properties
 fi
