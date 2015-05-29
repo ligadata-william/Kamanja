967eea2cab19f21379229fe104b1ae0e14655980
Pokuri <pokuri@ligadata.com>
2015-05-29 10:53:11 -0700

    Few more changes to compile models. Yet to fix lot more

--- trunk/SampleApplication/InterfacesSamples/src/main/scala/com/ligadata/models/Model1.scala
+++ trunk/SampleApplication/InterfacesSamples/src/main/scala/com/ligadata/models/Model1.scala
@@ -1,26 +1,33 @@
 package com.ligadata.models
 
+import com.ligadata.messagescontainers._
+import com.ligadata.FatafatBase.{ BaseMsg, BaseContainer, RDDBase, BaseContainerObj, MdBaseResolveInfo, MessageContainerBase, RDDObject, RDD, TimeRange, ModelBaseObj, ModelBase, ModelResult, TransactionContext }
+
 // Model1:
 
 // We generate an alert if min balance < 100 and if we haven't issued any 
 // alert today and if minBalanceAlertOptOut is N.
 
 object System_Model1_1000000 extends ModelBaseObj {
-  override def IsValidMessage(msg: MessageContainerBase): Boolean = ...
-  override def CreateNewModel(txnContext: TransactionContext): ModelBase = ...
+  override def IsValidMessage(msg: MessageContainerBase): Boolean = {
+    return false
+  }
+  override def CreateNewModel(txnContext: TransactionContext): ModelBase = {
+    return new System_Model1_1000000(txnContext)
+  }
 } 
 
 class System_Model1_1000000(txnContext: TransactionContext) extends ModelBase {
   override def execute(emitAllResults : Boolean) : ModelResult = {
-    val custInfo = ${System.CustomerInfo}.GetRecentRDDForCurrentPartitionKey
+    val custInfo = System_CustomerPreferences_1000000.GetRecentRDDForCurrentPartitionKey
     if (custInfo.isEmpty || custInfo.last.get.minbalancealertoptout != "N")
       return null
     val currentDate = current_date
-    val custAlertHistory = ${System.CustomerAlertHistory}.GetRecentRDDForCurrentPartitionKey
+    val custAlertHistory = System_AlertHistory_1000000.GetRecentRDDForCurrentPartitionKey
     if (custAlertHistory.isEmpty == false && currentDate <= custAlertHistory.last.get.lastalertdate) // today
       return null
-    val rcntTxn = ${System.TransactionMsg}.GetRecentRDDForCurrentPartitionKey
-    if (rcntTxn.isEmpty || txn.balance >= 100)
+    val rcntTxn = System_TransactionMsg_1000000.GetRecentRDDForCurrentPartitionKey
+    if (rcntTxn.isEmpty || rcntTxn.last.get.balance >= 100)
       return null
 
     // ... Prepare results here ...
