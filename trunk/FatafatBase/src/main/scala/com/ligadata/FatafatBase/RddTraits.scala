05e5dd2d5a9b1a793c07e8e84409b490bb08e54c
Pokuri <pokuri@ligadata.com>
2015-06-09 12:44:30 -0700

    Added little bit JavaRDD. Yet to add more functionality.

--- trunk/FatafatBase/src/main/scala/com/ligadata/FatafatBase/RddTraits.scala
+++ trunk/FatafatBase/src/main/scala/com/ligadata/FatafatBase/RddTraits.scala
@@ -7,6 +7,8 @@ import java.util.Date
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
+import com.ligadata.Exceptions._
+import com.ligadata.FatafatBase.api.java.function._
 
 class Stats {
   // # of Rows, Total Size of the data, Avg Size, etc
@@ -37,7 +39,7 @@ class PairRDDFunctions[K <: Any, V <: Any](self: RDD[(K, V)]) {
   val LOG = Logger.getLogger(getClass);
 
   def count: Long = self.size
-  
+
   def countByKey: Map[K, Long] = null
   def groupByKey: RDD[(K, Iterable[V])] = null
 
@@ -66,7 +68,7 @@ trait RDD[T <: Any] {
   def ++(other: RDD[T]): RDD[T] = this.union(other)
 
   // def sortBy[K](f: (T) => K, ascending: Boolean = true) (implicit ord: Ordering[K], ctag: ClassTag[K]): RDD[T] = this.keyBy[K](f).sortByKey(ascending, numPartitions).values
-        
+
   def intersection(other: RDD[T]): RDD[T]
 
   def groupBy[K](f: T => K): RDD[(K, Iterable[T])]
@@ -97,22 +99,82 @@ trait RDD[T <: Any] {
   def keyBy[K](f: T => K): RDD[(K, T)]
 }
 
+object JavaRDD {
+  implicit def fromRDD[T: ClassTag](rdd: RDD[T]): JavaRDD[T] = new JavaRDD[T](rdd)
+  implicit def toRDD[T](rdd: JavaRDD[T]): RDD[T] = rdd.rdd
+}
+
+abstract class AbstractJavaRDDLike[T, This <: JavaRDDLike[T, This]]
+  extends JavaRDDLike[T, This]
+
+trait JavaRDDLike[T, This <: JavaRDDLike[T, This]] {
+  def wrapRDD(rdd: RDD[T]): This
+
+  implicit val classTag: ClassTag[T]
+
+  def rdd: RDD[T]
+
+  def iterator: java.util.Iterator[T] = asJavaIterator(rdd.iterator)
+
+/*
+  def map[R](f: Function1[T, R]): JavaRDD[R] = new JavaRDD(rdd.map(f)(fakeClassTag))(fakeClassTag)
+
+  def flatMap[U](f: FlatMapFunction1[T, U]): JavaRDD[U] = {
+    import scala.collection.JavaConverters._
+    def fn = (x: T) => f.call(x).asScala
+    JavaRDD.fromRDD(rdd.flatMap(fn)(fakeClassTag[U]))(fakeClassTag[U])
+  }
+*/
+  
+  def count(): Long = rdd.count()
+
+  def size(): Long = rdd.size()
+
+  def first(): T = {
+    if (rdd.isEmpty)
+      throw new ObjectNotFoundException("Empty collection.")
+    rdd.first.get
+  }
+
+  def last(): T = {
+    if (rdd.isEmpty)
+      throw new ObjectNotFoundException("Empty collection.")
+    rdd.last.get
+  }
+
+  def isEmpty(): Boolean = rdd.isEmpty()
+}
+
+class JavaRDD[T](val rdd: RDD[T])(implicit val classTag: ClassTag[T])
+  extends AbstractJavaRDDLike[T, JavaRDD[T]] {
+
+  override def wrapRDD(rdd: RDD[T]): JavaRDD[T] = JavaRDD.fromRDD(rdd)
+
+  def union(other: JavaRDD[T]): JavaRDD[T] = wrapRDD(rdd.union(other.rdd))
+  
+  def intersection(other: JavaRDD[T]): JavaRDD[T] = wrapRDD(rdd.intersection(other.rdd))
+  
+  def subtract(other: JavaRDD[T]): JavaRDD[T] = wrapRDD(rdd.subtract(other))
+
+  // def sortBy[S](f: JFunction1[T, S], ascending: Boolean): JavaRDD[T]
+}
+
 trait RDDObject[T <: Any] {
   val LOG = Logger.getLogger(getClass);
 
   // get builder
-  def build : T 
-  def build(from: T) : T 
+  def build: T
+  def build(from: T): T
 
   // First group of functions retrieve one object (either recent or for a given key & filter)
   // Get recent entry for the Current Key
-  def getRecent: Option[T] 
+  def getRecent: Option[T]
   def getRecentOrDefault: T
-  
+
   // Get recent entry for the given key
   def getRecent(key: Array[String]): Option[T]
   def getRecentOrDefault(key: Array[String]): T
-  
+
   def getOne(tmRange: TimeRange, f: T => Boolean): Option[T]
   def getOne(key: Array[String], tmRange: TimeRange, f: T => Boolean): Option[T]
   def getOneOrDefault(key: Array[String], tmRange: TimeRange, f: T => Boolean): T
@@ -121,15 +183,15 @@ trait RDDObject[T <: Any] {
   // This group of functions retrieve collection of objects 
   def getRDDForCurrKey(f: T => Boolean): RDD[T]
   def getRDDForCurrKey(tmRange: TimeRange, f: T => Boolean): RDD[T]
-  
+
   // With too many messages, these may fail - mostly useful for message types where number of messages are relatively small 
-  def getRDD(tmRange: TimeRange, func: T => Boolean) : RDD[T]
-  def getRDD(tmRange: TimeRange) : RDD[T]
-  def getRDD(func: T => Boolean) : RDD[T]
-  
-  def getRDD(key: Array[String], tmRange: TimeRange, func: T => Boolean) : RDD[T]
-  def getRDD(key: Array[String], func: T => Boolean) : RDD[T]
-  def getRDD(key: Array[String], tmRange: TimeRange) : RDD[T]
+  def getRDD(tmRange: TimeRange, func: T => Boolean): RDD[T]
+  def getRDD(tmRange: TimeRange): RDD[T]
+  def getRDD(func: T => Boolean): RDD[T]
+
+  def getRDD(key: Array[String], tmRange: TimeRange, func: T => Boolean): RDD[T]
+  def getRDD(key: Array[String], func: T => Boolean): RDD[T]
+  def getRDD(key: Array[String], tmRange: TimeRange): RDD[T]
 
   // Saving data
   def saveOne(inst: T) = {}
@@ -139,11 +201,11 @@ trait RDDObject[T <: Any] {
 
 object StringUtils {
   val emptyStr = ""
-  val quotes3  = "\"\"\""
-  val quotes2  = "\"\""
-  val spaces2  = "  "
-  val spaces3  = "   "
-  val spaces4  = spaces3 + " "
+  val quotes3 = "\"\"\""
+  val quotes2 = "\"\""
+  val spaces2 = "  "
+  val spaces3 = "   "
+  val spaces4 = spaces3 + " "
 
   val tab1 = spaces2
   val tab2 = tab1 + tab1
@@ -167,36 +229,36 @@ object StringUtils {
   val lfTab9 = lfTab8 + tab1
 
   // return zero for NULL and empty strings, for others length of the given string
-  def len(str : String) = str match { case null => 0; case _ => str.length }
+  def len(str: String) = str match { case null => 0; case _ => str.length }
   def emptyIfNull(str: String) = str match { case null => ""; case _ => str }
-  def emptyIfNull(str: String, fn : String => String) = str match { case null => ""; case _ => fn(str) }
+  def emptyIfNull(str: String, fn: String => String) = str match { case null => ""; case _ => fn(str) }
 
-  def catStrs(strs: Array[String], sep: String, fn : String => String) = {
+  def catStrs(strs: Array[String], sep: String, fn: String => String) = {
     val sb = new StringBuilder();
-    strs.foldLeft(false)((f, s) => {if(f) sb.append(sep); sb.append(emptyIfNull(s, fn)); true});
+    strs.foldLeft(false)((f, s) => { if (f) sb.append(sep); sb.append(emptyIfNull(s, fn)); true });
     sb.result
   }
 
-  def catStrs(strs: Array[String], sep: String) : String = catStrs(strs, sep, { str : String => str })
+  def catStrs(strs: Array[String], sep: String): String = catStrs(strs, sep, { str: String => str })
 
   // returns the given string with its first character in lower case. no checks for null or empty string
-  private def firstLowerNoCheck(str : String) = { str.charAt(0).toLower + str.substring(1) }
+  private def firstLowerNoCheck(str: String) = { str.charAt(0).toLower + str.substring(1) }
 
   // returns a new string with its first character in lower case  and rest unchanged (null or empty string returns the given string)
-  def firstLower(str : String) = len(str) match { case 0 => str; case _ => firstLowerNoCheck(str) }
+  def firstLower(str: String) = len(str) match { case 0 => str; case _ => firstLowerNoCheck(str) }
 
-  def AddAsLinesToBuf(buf: StringBuilder, lines : Array[String]) =  lines.foreach(line => buf.append(line).append("\n"))
-  def firstNonNull(args: String*) : String = { if(args == null) return null; for (arg <- args) {  if (arg != null) return arg };  null; }
+  def AddAsLinesToBuf(buf: StringBuilder, lines: Array[String]) = lines.foreach(line => buf.append(line).append("\n"))
+  def firstNonNull(args: String*): String = { if (args == null) return null; for (arg <- args) { if (arg != null) return arg }; null; }
 
-  def prefixSpaces(str: String) : String = {
-    val cnt = str.prefixLength({ch => ( ch == ' ') || (ch == '\t')});
-    if(cnt == 0) emptyStr
+  def prefixSpaces(str: String): String = {
+    val cnt = str.prefixLength({ ch => (ch == ' ') || (ch == '\t') });
+    if (cnt == 0) emptyStr
     else str.substring(0, cnt)
   }
 
-  def lastNonEmptyLine(strs : Array[String]) : String = {
+  def lastNonEmptyLine(strs: Array[String]): String = {
     var idx = strs.length
-    while (idx > 0) {  idx -= 1; val str = strs(idx); if(str.length > 0) return str; }
+    while (idx > 0) { idx -= 1; val str = strs(idx); if (str.length > 0) return str; }
     emptyStr
   }
 }
@@ -208,26 +270,26 @@ object RddDate {
   def currentDateTime = RddDate(GetCurDtTmInMs)
 }
 
-case class RddDate(tms : Long) extends Date (tms) {
+case class RddDate(tms: Long) extends Date(tms) {
   def timeDiffInHrs(dt: Date): Int = 0
   def timeDiffInHrs(dt: RddDate): Int = 0
-  def lastNdays(days: Int) : TimeRange = null;
+  def lastNdays(days: Int): TimeRange = null;
 }
 
 object RddUtils {
-  def IsEmpty(str : String) = (str == null || str.length == 0)
-  def IsNotEmpty(str : String) = (! IsEmpty(str))
-  def IsEmptyArray[T](a : Array[T]) = (a == null || a.size == 0)
-  def IsNotEmptyArray[T](a : Array[T]) = (! IsEmptyArray(a))
+  def IsEmpty(str: String) = (str == null || str.length == 0)
+  def IsNotEmpty(str: String) = (!IsEmpty(str))
+  def IsEmptyArray[T](a: Array[T]) = (a == null || a.size == 0)
+  def IsNotEmptyArray[T](a: Array[T]) = (!IsEmptyArray(a))
 
   def SimpDateFmtTimeFromMs(tmMs: Long, fmt: String): String = { new java.text.SimpleDateFormat(fmt).format(new java.util.Date(tmMs)) }
 
   // Get current time (in ms) as part of valid identifier and appends to given string
-  def GetCurDtTmAsId(prefix: String) : String = { prefix + SimpDateFmtTimeFromMs(GetCurDtTmInMs, "yyyyMMdd_HHmmss_SSS") }
-  def GetCurDtTmAsId : String = GetCurDtTmAsId("")
-  
+  def GetCurDtTmAsId(prefix: String): String = { prefix + SimpDateFmtTimeFromMs(GetCurDtTmInMs, "yyyyMMdd_HHmmss_SSS") }
+  def GetCurDtTmAsId: String = GetCurDtTmAsId("")
+
   def GetCurDtTmStr: String = { SimpDateFmtTimeFromMs(GetCurDtTmInMs, "yyyy:MM:dd HH:mm:ss.SSS") }
   def GetCurDtStr: String = { SimpDateFmtTimeFromMs(GetCurDtTmInMs, "yyyy:MM:dd") }
-  def GetCurDtTmInMs: Long =  { System.currentTimeMillis }
+  def GetCurDtTmInMs: Long = { System.currentTimeMillis }
   def elapsed[A](f: => A): (Long, A) = { val s = System.nanoTime; val ret = f; ((System.nanoTime - s), ret); }
 }
