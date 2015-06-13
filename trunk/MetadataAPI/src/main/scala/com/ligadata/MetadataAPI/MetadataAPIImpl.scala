2321ccd80e5fd102fa0f3c490e876d08d8d7c316
System Administrator <root@Dans-MacBook-Pro.local>
2015-06-12 20:06:16 -0700

    NewAPI: Create a version-less jar file when compiling Messages/Containers

--- trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/MetadataAPIImpl.scala
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/MetadataAPIImpl.scala
@@ -2583,7 +2583,7 @@ object MetadataAPIImpl extends MetadataAPI {
     try {
       var compProxy = new CompilerProxy
       compProxy.setLoggerLevel(Level.TRACE)
-      val (classStr, cntOrMsgDef) = compProxy.compileMessageDef(contOrMsgText,recompile)
+      val (classStrVer, cntOrMsgDef, classStrNoVer) = compProxy.compileMessageDef(contOrMsgText,recompile)
       logger.debug("Message/Container Compiler returned an object of type " + cntOrMsgDef.getClass().getName())
       cntOrMsgDef match {
         case msg: MessageDef => {
@@ -2728,7 +2728,7 @@ object MetadataAPIImpl extends MetadataAPI {
     try {
       var compProxy = new CompilerProxy
       compProxy.setLoggerLevel(Level.TRACE)
-      val (classStr, msgDef) = compProxy.compileMessageDef(messageText)
+      val (classStrVer, msgDef, classStrNoVer) = compProxy.compileMessageDef(messageText)
       val key = msgDef.FullNameWithVer
       msgDef match {
         case msg: MessageDef => {
