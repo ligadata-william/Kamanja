813125c458b52119c86ee6a412d92584359e6398
Pokuri <pokuri@ligadata.com>
2015-06-16 12:01:45 -0700

    Making ModelBase and ModelBaseObj in single file for Java implementation.

--- trunk/SampleApplication/InterfacesSamples/src/main/java/com/ligadata/samples/models/JavaTestMdl.java
+++ trunk/SampleApplication/InterfacesSamples/src/main/java/com/ligadata/samples/models/JavaTestMdl.java
@@ -5,43 +5,47 @@ import com.ligadata.samples.messages.*;
 import com.google.common.base.Optional;
 
 public class JavaTestMdl extends ModelBase {
+	public static class JavaTestMdlObj implements ModelBaseObj {
+		public boolean IsValidMessage(MessageContainerBase msg) {
+			return (msg instanceof CustAlertHistory);
+		}
+
+		public ModelBase CreateNewModel(TransactionContext txnContext) {
+			return new JavaTestMdl(txnContext);
+		}
+
+		public String ModelName() {
+			return "JavaTestMdl";
+		}
+
+		public String Version() {
+			return "0.0.1";
+		}
+	}
+
 	static JavaTestMdlObj objSignleton = new JavaTestMdlObj();
 
 	public ModelResult execute(boolean emitAllResults) {
-		// Directly calling methods from Scala Singleton object. Not preferable to use direct scala.
-		CustAlertHistory custAlertHistory = CustAlertHistory$.MODULE$.getRecentOrNew();
+		// Directly calling methods from Scala Singleton object. Not preferable
+		// to use direct scala.
+		CustAlertHistory custAlertHistory = CustAlertHistory$.MODULE$
+				.getRecentOrNew();
 
 		// Getting Java RDD Object and performing operations on that
-		JavaRDDObject<CustAlertHistory> javaRddObj = CustAlertHistory$.MODULE$.toJavaRDDObject();
+		JavaRDDObject<CustAlertHistory> javaRddObj = CustAlertHistory$.MODULE$
+				.toJavaRDDObject();
 		Optional<CustAlertHistory> obj = javaRddObj.getRecent();
-		
+
 		if (obj.isPresent()) {
-			
+
 		} else {
-			
+
 		}
-		
-		
+
 		return null;
 	}
 
 	public JavaTestMdl(TransactionContext inTxnContext) {
 		super(new ModelContext(inTxnContext), objSignleton);
-	}
-
-	public static boolean IsValidMessage(MessageContainerBase msg) {
-		return (msg instanceof CustAlertHistory);
-	}
-
-	public static ModelBase CreateNewModel(TransactionContext txnContext) {
-		return new JavaTestMdl(txnContext);
-	}
-
-	public static String getModelName() {
-		return "LowBalanceAlert";
-	}
-
-	public static String getVersion() {
-		return "0.0.1";
 	}
 }
