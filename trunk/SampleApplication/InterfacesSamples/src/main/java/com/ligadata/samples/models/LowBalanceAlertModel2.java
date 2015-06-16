ed45190f0f100b909a6703fc27c45df55bed60aa
System Administrator <root@Dans-MacBook-Pro.local>
2015-06-15 18:56:22 -0700

    NewAPI: Some java models

---
+++ trunk/SampleApplication/InterfacesSamples/src/main/java/com/ligadata/samples/models/LowBalanceAlertModel2.java
@@ -0,0 +1,128 @@
+package com.ligadata.samples.models;
+
+import com.ligadata.FatafatBase.*;
+import com.ligadata.FatafatBase.api.java.function.*;
+import com.ligadata.samples.messages.*;
+import com.google.common.base.Optional;
+
+public class LowBalanceAlertModel2 extends ModelBase { 
+
+	static LowBalanceAlertModel2Obj objSignleton = new LowBalanceAlertModel2Obj();
+	private TransactionContext txnContext = null;
+	
+    // Collections/messages to be used in this model
+    private GlobalPreferences gPref = null;
+    private CustPreferences pref = null;
+    private JavaRDD<CustTransaction> rcntTxns = null;
+    private RddDate curDt = RddDate.currentDateTime();
+    private CustAlertHistory alertHistory = null;
+    
+    
+    public ModelResult execute(boolean emitAllResults) {
+        
+        // First check the preferences and decide whether to continue or not
+    	gPref = GlobalPreferences.getRecentOrNew();
+        pref = CustPreferences.getRecentOrNew();
+        
+        if (pref.minBalanceAlertOptout() == false)
+          return null;
+
+        // Check if at least min number of hours elapsed since last alert  
+        alertHistory = CustAlertHistory.getRecentOrNew();
+        
+        if (curDt.timeDiffInHrs(alertHistory.alertDt()) < gPref.minAlertDurationInHrs())
+          return null;
+        
+        // get history of transaction whose balance is less than minAlertBalance in last N days
+        TimeRange lookBackTime = curDt.lastNdays(gPref.numLookbackDaysForMultiDayMinBalanceAlert());
+        rcntTxns = CustTransaction$.MODULE$.toJavaRDDObject().getRDD(lookBackTime, new MinBalanceDeterminator());
+        
+        // Getting Java RDD Object and performing operations on that
+        if (!rcntTxns.isEmpty() || rcntTxns.count() < gPref.maxNumDaysAllowedWithMinBalance() ) {
+          return null; 	
+        }
+        
+        // compute distinct days in the retrieved history (number of days with balance less than minAlertBalance)
+        // and check if those days meet the threshold
+        int daysWhenBalanceIsLessThanMin = (rcntTxns.groupBy(new TransactionCorrelator())).count;
+        		
+        if(daysWhenBalanceIsLessThanMin <= gPref.maxNumDaysAllowedWithMinBalance())
+          return null;
+
+        // create new alert history record and persist (if policy is to keep only one, this will replace existing one)
+        CustAlertHistory.build().withAlertDt(curDt).withAlertType("lowBalanceAlert").save();
+        
+        // ... Prepare results here ... need to populate result object with appropriate attributes
+        return ModelResult.builder().withResult(new LowBalanceAlertResult2(txnContext)).build(); 
+    }
+    
+    
+    /**
+     * @param inTxnContext
+     */
+    public LowBalanceAlertModel2 (TransactionContext inTxnContext) {
+    	super(new ModelContext(inTxnContext), objSignleton);
+    }
+
+    /**
+      * 
+      * @param msg
+      * @return
+      */	 
+    public static boolean IsValidMessage(MessageContainerBase msg) {
+  	  return (msg instanceof CustTransaction);
+    }
+       
+       
+    /**
+    *  
+    * @param txnContext
+    * @return
+    */
+    public static LowBalanceAlertModel2 CreateNewModel(TransactionContext txnContext) {
+      return new LowBalanceAlertModel2(txnContext);  	  
+    }
+ 
+    /**
+     * 
+     * @return
+     */
+	public static String getModelName() {
+		return "LowBalanceAlertModel2";
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static String getVersion() {
+		return "0.0.1";
+	}
+	
+    // This is the object that needs to be implemented to 
+    class LowBalanceAlertResult2 {
+    	private TransactionContext cxt;
+    	public LowBalanceAlertResult2(TransactionContext inTxnContext) {
+          cxt = inTxnContext;
+    	}
+    }
+    
+    // Implement this to use getRDD method on RDDObject
+    class MinBalanceDeterminator implements Function1<CustTransaction, Boolean> {   	
+      public Boolean call (CustTransaction ct) {
+        //{ trn : CustTransaction => trn.balance < gPref.minAlertBalance }
+        if (ct.balance() < gPref.minAlertBalance()) {
+          return true;	  
+        }
+        return false;
+      }
+    }
+    
+    class TransactionCorrelator implements Function1<CustTransaction, Integer> {
+      public Integer call(CustTransaction ct) {   
+        return new Integer(ct.date());
+      } 	
+    }
+	
+	
+}
---
+++ trunk/SampleApplication/InterfacesSamples/src/main/java/com/ligadata/samples/models/LowBalanceAlertModelObj.java
@@ -0,0 +1,24 @@
+package com.ligadata.samples.models;
+
+import com.ligadata.FatafatBase.*;
+import com.ligadata.samples.messages.*;
+
+public class LowBalanceAlertModelObj implements ModelBaseObj {
+	
+	public boolean IsValidMessage(MessageContainerBase msg) {
+		return (msg instanceof CustTransaction);
+	}
+	
+	public ModelBase CreateNewModel(TransactionContext txnContext) {
+		return new LowBalanceAlertModel(txnContext);
+	}
+
+	public String ModelName() {
+		return "JavaTestMdl";
+	}
+
+	public String Version() {
+		return "0.0.1";
+	}
+
+}
