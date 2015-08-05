db62cf9aedee2193e380271994c781a6c4ac6dcd
Daniel Kozin <danielkozin@Daniels-MacBook-Pro.local>
2015-08-04 17:16:24 -0700

    change the name of the Metadata Node ZNode to /NodeX

--- trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/MetadataAPIImpl.scala
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/MetadataAPIImpl.scala
@@ -135,7 +135,7 @@ object MetadataAPIImpl extends MetadataAPI {
                                "JAR_PATHS","JAR_TARGET_DIR","ROOT_DIR","GIT_ROOT","SCALA_HOME","JAVA_HOME","MANIFEST_PATH","CLASSPATH","NOTIFY_ENGINE","SERVICE_HOST",
                                "ZNODE_PATH","ZOOKEEPER_CONNECT_STRING","COMPILER_WORK_DIR","SERVICE_PORT","MODEL_FILES_DIR","TYPE_FILES_DIR","FUNCTION_FILES_DIR",
                                "CONCEPT_FILES_DIR","MESSAGE_FILES_DIR","CONTAINER_FILES_DIR","CONFIG_FILES_DIR","MODEL_EXEC_LOG","NODE_ID","SSL_CERTIFICATE","SSL_PASSWD", "DO_AUTH","SECURITY_IMPL_CLASS",
-                               "SECURITY_IMPL_JAR", "AUDIT_IMPL_CLASS","AUDIT_IMPL_JAR", "DO_AUDIT","AUDIT_PARMS", "ADAPTER_SPECIFIC_CONFIG","METADATANODEID")
+                               "SECURITY_IMPL_JAR", "AUDIT_IMPL_CLASS","AUDIT_IMPL_JAR", "DO_AUDIT","AUDIT_PARMS", "ADAPTER_SPECIFIC_CONFIG")
   var isCassandra = false
   private[this] val lock = new Object
   var startup = false
@@ -172,7 +172,7 @@ object MetadataAPIImpl extends MetadataAPI {
    */
   def clockNewActivity: Unit= {
     if (heartBeat != null)
-      heartBeat.SetMainData("MetadataNodeID" + metadataAPIConfig.getProperty("METADATANODEID").toString)
+      heartBeat.SetMainData("Node" + metadataAPIConfig.getProperty("NODE_ID").toString)
   }
 
 
@@ -6266,7 +6266,7 @@ object MetadataAPIImpl extends MetadataAPI {
     if (zkHeartBeatNodePath.size > 0) {  
       heartBeat = new HeartBeatUtil
       heartBeat.Init("Metadata", metadataAPIConfig.getProperty("ZOOKEEPER_CONNECT_STRING"), zkHeartBeatNodePath,3000, 3000, 5000) // for every 5 secs
-      heartBeat.SetMainData("MetadataNodeID" + metadataAPIConfig.getProperty("METADATANODEID").toString)
+      heartBeat.SetMainData("Node" + metadataAPIConfig.getProperty("NODE_ID").toString)
       MonitorAPIImpl.startMetadataHeartbeat
     }
 
