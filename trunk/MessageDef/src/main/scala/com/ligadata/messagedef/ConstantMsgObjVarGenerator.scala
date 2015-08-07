b81c283e24ad071c275dc7482b70c031280dd86d
Daniel Kozin <danielkozin@Daniels-MacBook-Pro.local>
2015-08-07 13:20:34 -0700

    Missed a file for the merge

--- trunk/MessageDef/src/main/scala/com/ligadata/messagedef/ConstantMsgObjVarGenerator.scala
+++ trunk/MessageDef/src/main/scala/com/ligadata/messagedef/ConstantMsgObjVarGenerator.scala
@@ -376,7 +376,6 @@ class ConstantMsgObjVarGenerator {
     if (msg.msgtype.equals("Message"))
       imprt = "import com.ligadata.KamanjaBase.{BaseMsg, BaseMsgObj, TransformMessage, BaseContainer, MdBaseResolveInfo, MessageContainerBase, RDDObject, RDD, TimeRange, JavaRDDObject}"
     else if (msg.msgtype.equals("Container"))
-
       imprt = "import com.ligadata.KamanjaBase.{BaseMsg, BaseContainer, BaseContainerObj, MdBaseResolveInfo, MessageContainerBase, RDDObject, RDD, TimeRange, JavaRDDObject}"
     var nonVerPkg = "package " + msg.pkg +";\n"
     var verPkg = "package " + msg.pkg + ".V" + MdMgr.ConvertVersionToLong(msg.Version).toString + ";\n"
