538f243e3c89655553b46e4e015b68593daf7cbc
ligadata-eshan <eshan@ligadata.com>
2015-08-09 19:24:53 -0700

    Added Metadata and other templates, modified easyinstall to install telecom app

---
+++ trunk/SampleApplication/Telecom/metadata/template/script/StartMetadataAPI_Template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ipport="8998"
+
+if [ "$1" != "debug" ]; then
+	java -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/input/Telecom/metadata/config/MetadataAPIConfig.properties
+else	
+	java -Xdebug -Xrunjdwp:transport=dt_socket,address="$ipport",server=y -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/input/Telecom/metadata/config/MetadataAPIConfig.properties
+fi
+
