4902cd0f5ead0b3a3bab7234de660d1c6d8fd851
Dhaval Kolapkar <dhaval@ligadata.com>
2015-08-11 16:08:52 -0700

    Add container api to the utility

--- trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/Utility/Action.scala
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/Utility/Action.scala
@@ -6,10 +6,17 @@ package main.scala.com.ligadata.MetadataAPI.Utility
  */
 object Action extends Enumeration {
   type Action = Value
+  //message manangemen
   val ADDMESSAGE=Value("addmessage")
   val UPDATEMESSAGE=Value("updatemessage")
   val GETALLMESSAGES=Value("getallmessages")
   val REMOVEMESSAGE=Value("removemessage")
+  //output message management
+  val ADDOUTPUTMESSAGE=Value("addoutputmessage")
+  val UPDATEOUTPUTMESSAGE=Value("updateoutputmessage")
+  val REMOVEOUTPUTMESSAGE=Value("removeoutputmessage")
+  val GETALLOUTPUTMESSAGES=Value("getalloutputmessages")
+  //model management
   val ADDMODELPMMML=Value("addmodelpmml")
   val ADDMODELSCALA=Value("addmodelscala")
   val ADDMODELJAVA=Value("addmodeljava")
@@ -19,9 +26,12 @@ object Action extends Enumeration {
   val UPDATEMODEL=Value("updatemodel")
   val GETALLMODELS=Value("getallmodels")
   val GETMODEL=Value("getmodel")
-  val ADDOUTPUTMESSAGE=Value("addoutputmessage")
-  val UPDATEOUTPUTMESSAGE=Value("updateoutputmessage")
-  val REMOVEOUTPUTMESSAGE=Value("removeoutputmessage")
-  val GETALLOUTPUTMESSAGES=Value("getalloutputmessages")
+//container management
+  val ADDCONTAINER = Value("addcontainer")
+  val UPDATECONTAINER = Value("updatecontainer")
+  val GETCONTAINER = Value("getcontainer")
+  val GETALLCONTAINERS= Value("getallcontainers")
+  val REMOVECONTAINER= Value("removecontainer")
+
 }
 
