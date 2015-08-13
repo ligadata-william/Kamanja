20218a941bc62f3090e4c9788a7102ee924f215f
Dhaval Kolapkar <dhaval@ligadata.com>
2015-08-13 11:16:38 -0700

    Add Concepts, Dump, Function, Jars and Model service.

---
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/Utility/DumpService.scala
@@ -0,0 +1,28 @@
+package main.scala.com.ligadata.MetadataAPI.Utility
+
+/**
+ * Created by dhaval on 8/13/15.
+ */
+object DumpService {
+  def dumpMetadata: String ={
+var response="TODO"
+
+    response
+  }
+  def dumpAllNodes: String ={
+    var response="TODO"
+    response
+  }
+  def dumpAllClusters: String ={
+    var response="TODO"
+    response
+  }
+  def dumpAllClusterCfgs: String ={
+    var response="TODO"
+    response
+  }
+  def dumpAllAdapters: String ={
+    var response="TODO"
+    response
+  }
+}
