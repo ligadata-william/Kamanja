c12d8b07e4854fa521f2fe5b015fad9331499097
ligadata-eshan <eshan@ligadata.com>
2015-08-17 10:37:00 -0700

    missed some changes: changing fatafat to kamanja

--- trunk/SampleApplication/HelloWorld/template/script/PushSampleDataToKafka_Template.sh
+++ trunk/SampleApplication/HelloWorld/template/script/PushSampleDataToKafka_Template.sh
@@ -1,1 +1,1 @@
-java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/application-1-HelloWorld/data/HelloWorld_Input_Data.csv.gz" --partitionkeyidxs "1" --format CSV
+java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/HelloWorld/data/HelloWorld_Input_Data.csv.gz" --partitionkeyidxs "1" --format CSV
