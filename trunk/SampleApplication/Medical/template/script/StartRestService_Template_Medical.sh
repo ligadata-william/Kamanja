73fb4b7d668219334ec873c3bedf69c9ccd6398d
ligadata-tulasi <tulasi@ligadata.com>
2015-08-25 16:43:39 -0700

    Deleting the unused templates

--- trunk/SampleApplication/Medical/template/script/StartRestService_Template_Medical.sh
+++
@@ -1,1 +0,0 @@
-java -jar {InstallDirectory}/bin/MetadataAPIService-1.0 --config {InstallDirectory}/input/SampleApplications/metadata/config/MetadataAPIConfig_Medical.properties
--- trunk/SampleApplication/Telecom/metadata/template/script/PushSampleDataToKafka_Template_Telecom.sh
+++
@@ -1,3 +0,0 @@
-# java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/LowBalance/data/TxnDataOneCustomer.dat" --partitionkeyidxs "1" --format CSV
-
-java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/SampleApplications/data/SubscriberUsage_Telecom.dat.gz" --partitionkeyidxs "1" --format CSV
