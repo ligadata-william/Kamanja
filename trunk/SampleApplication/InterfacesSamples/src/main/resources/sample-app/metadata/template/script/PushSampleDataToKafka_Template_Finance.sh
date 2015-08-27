03b9cb7b3f6130fbe6bdf3b1899f861392c70522
ligadata-william <william@ligadata.com>
2015-08-26 18:48:41 -0700

    Adding push data scripts for all sample applications

--- trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/PushSampleDataToKafka_Template_Finance.sh
+++ trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/PushSampleDataToKafka_Template_Finance.sh
@@ -1,3 +1,2 @@
-# java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/SampleApplications/data/TxnDataOneCustomer.dat" --partitionkeyidxs "1" --format CSV
-
-java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/SampleApplications/data/TxnData_Finance.dat" --partitionkeyidxs "1" --format CSV
+#!/usr/bin/env bash
+java -jar {InstallDirectory}/bin/SimpleKafkaProducer-0.1.0 --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "{InstallDirectory}/input/SampleApplications/data/TxnData_Finance.dat.gz" --partitionkeyidxs "1" --format CSV
