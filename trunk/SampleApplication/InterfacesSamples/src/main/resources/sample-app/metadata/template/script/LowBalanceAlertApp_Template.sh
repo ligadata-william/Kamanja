bee245a7aa6286e45ce9692697c221fedb446de8
ligadata-william <william@ligadata.com>
2015-09-04 11:55:32 -0700

    Modified installation and script paths to copy configuration files to the correct location and configuration files to properly point to that location as well. Also changed installation scripts to use the same config file at $KAMANJA_HOME/config/MetadataAPIConfig.properties.

--- trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/LowBalanceAlertApp_Template.sh
+++ trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/LowBalanceAlertApp_Template.sh
@@ -2,18 +2,18 @@
 
 KAMANJA_HOME={InstallDirectory}
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties upload engine config $KAMANJA_HOME/config/ClusterConfig.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties upload engine config $KAMANJA_HOME/config/ClusterConfig.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustAlertHistory_Finance.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustAlertHistory_Finance.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustPreferences_Finance.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustPreferences_Finance.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustomerInfo_Finance.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustomerInfo_Finance.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/GlobalPreferences_Finance.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/GlobalPreferences_Finance.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties add message $KAMANJA_HOME/input/SampleApplications/metadata/message/TransactionMsg_Finance.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add message $KAMANJA_HOME/input/SampleApplications/metadata/message/TransactionMsg_Finance.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties upload compile config $KAMANJA_HOME/input/SampleApplications/metadata/config/LBCompileCfg_Finance.json
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties upload compile config $KAMANJA_HOME/config/LBCompileCfg_Finance.json
 
-$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/input/SampleApplications/metadata/config/MetadataAPIConfig_HelloWorld.properties add model scala $KAMANJA_HOME/input/SampleApplications/metadata/model/LowBalanceAlert_Finance.scala DEPENDSON lowbalancealert
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add model scala $KAMANJA_HOME/input/SampleApplications/metadata/model/LowBalanceAlert_Finance.scala DEPENDSON lowbalancealert
