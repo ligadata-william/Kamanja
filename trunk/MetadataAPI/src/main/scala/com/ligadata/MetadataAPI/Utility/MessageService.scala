629eaac24cba74689c337920b97d8d296fde4b2a
Daniel Kozin <danielkozin@Daniels-MacBook-Pro.local>
2015-09-08 16:35:41 -0700

    remove the version from the namespace

--- trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/Utility/MessageService.scala
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/Utility/MessageService.scala
@@ -331,9 +331,7 @@ object MessageService {
 
         val msgKey = outputMessageKeys(choice - 1)
         val msgKeyTokens = msgKey.split("\\.")
-        val msgNameSpace = msgKeyTokens(0)
-        val msgName = msgKeyTokens(1)
-        val msgVersion = msgKeyTokens(2)
+        val(msgNameSpace, msgName, msgVersion) = com.ligadata.kamanja.metadata.Utils.parseNameToken(msgKey)
         val apiResult = MetadataAPIOutputMsg.RemoveOutputMsg(msgNameSpace, msgName, msgVersion.toLong, userid).toString
         response = apiResult
       }
