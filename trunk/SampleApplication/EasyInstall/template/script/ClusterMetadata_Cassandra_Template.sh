761a2fa6a8e55a20281311913533485cebf02d07
Pokuri <pokuri@ligadata.com>
2015-12-01 14:47:20 -0800

    Changed from -Dlog4j.configuration to -Dlog4j.configurationFile And refreshing log files for every 5 mins by default

--- trunk/SampleApplication/EasyInstall/template/script/ClusterMetadata_Cassandra_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/script/ClusterMetadata_Cassandra_Template.sh
@@ -6,9 +6,9 @@
 ipport="8998"
 
 if [ "$1" != "debug" ]; then
-	java -Dlog4j.configuration=file:{InstallDirectory}/config/log4j.properties -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/config/ClusterCfgMetadataAPIConfig_Cassandra.properties
+	java -Dlog4j.configurationFile=file:{InstallDirectory}/config/log4j.properties -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/config/ClusterCfgMetadataAPIConfig_Cassandra.properties
 else
-	java -Xdebug -Xrunjdwp:transport=dt_socket,address="$ipport",server=y -Dlog4j.configuration=file:{InstallDirectory}/config/log4j.properties -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/config/ClusterCfgMetadataAPIConfig_Cassandra.properties
+	java -Xdebug -Xrunjdwp:transport=dt_socket,address="$ipport",server=y -Dlog4j.configurationFile=file:{InstallDirectory}/config/log4j.properties -jar {InstallDirectory}/bin/MetadataAPI-1.0 --config {InstallDirectory}/config/ClusterCfgMetadataAPIConfig_Cassandra.properties
 fi
 
 
